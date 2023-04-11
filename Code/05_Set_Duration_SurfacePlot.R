11e987345698e8334a1c6f0a12a2c770759723b3
Damie Pak <dkp5177@psu.edu>
2023-04-11 16:59:27 -0400

    Set Duration code was totally reformated and works without having to cut it up into so many little chunks

--- Code/05_Set_Duration_SurfacePlot.R
+++ Code/05_Set_Duration_SurfacePlot.R
@@ -1,228 +1,61 @@
 ##################
 ###Set durations##
 ##################
+###Burst Size Versus Transmission Investment ###
+B_V = seq(1, 50, 0.5) #Burst size
+C_V = seq(.01, 1, 0.01) #Transmission investment 
+B_V_C_V <- expand.grid(B_V = B_V, C_V = C_V) #Different combination 
 
-###The set duration is a supplementary analysis to show that
-###our way of choosing the end of the acute phase works.
+###I am able to figure out which of the burst and transmission 
+###are unable to establish the infection
+p_val = 2.5e-6
+mu_M = 48
+R_val = 8500000
 
-
-############
-###DAY 10###
-############
-
-Fitness_MODEL_PC_10<- lapply(FULL_MODEL_PC, function(x) subset(as.data.frame(x),
-                                                               as.data.frame(x)$time <= 10))
-
-Fitness_Cut_PC_10_cp <- data.frame(fitness=do.call(rbind,
-                                                   lapply(Fitness_MODEL_PC_10 ,
-                                                          Gametocyte_Fitness,"PC")))
-
-###The maximum fitness is Burst size =50, transmission investment
-###90%.
+RM_limit_1 <- ((p_val * R_val) + mu_M)/(p_val * R_val)
 
 
-Fitness_Cut_PC_10_cp$B_V <- R_C_V$B_V
-Fitness_Cut_PC_10_cp$C_V <- R_C_V$C_V 
-Fitness_Cut_PC_10_cp$Death <- FULL_T_F_PC$TF.V2
-
-
-
-GG_Fitness_Cut_PC_10_cp <- 
-  ggplot(Fitness_Cut_PC_10_cp, aes(x=B_V, y= C_V))+
-  geom_raster(aes(fill = fitness))+
-  geom_raster(data=unestablished_index ,aes(x=B_V, y=C_V),
-              fill='#DBDEE8')+
-  geom_raster(data=mort_fitness,aes(x= B_V, y=C_V,
-                                    fill= endfitness))+
-  geom_segment(data=vert_seg_mort, 
-               aes(x=x, xend=xend,
-                   y=y, yend=yend),color='#31f5a3',size=1.2,
-               lineend='round')+
-  geom_segment(data=hor_seg_mort, 
-               aes(x=x, xend=xend,
-                   y=y, yend=yend),color='#31f5a3',size=1.2,
-               lineend='round')+
-  geom_segment(data=vert_seg, 
-               aes(x=x, xend=xend,
-                   y=y, yend=yend),color='black',size=0.8,lineend='round')+
-  geom_segment(data=hor_seg, 
-               aes(x=x, xend=xend,
-                   y=y, yend=yend),color='black',size=0.8,
-               lineend='round')+
-  scale_fill_viridis(name="Cumulative fitness",option='inferno',limits=c(0,45))+
-  scale_x_continuous(expand=c(0,0))+
-  scale_y_continuous(expand=c(0,0))+
-  xlab(expression("Burst size" ~beta))+
-  ylab("Transmission investment (c)")+  ggtitle("A.Day 10")+
-  theme(axis.text= element_text(size=14,color='black'),
-        axis.title= element_text(size=14,color='black'))+
-  annotate("point",x=50, y = 0.90,size=3,col='red')
-
-
+B_V_C_V$Establish <- ifelse((1-B_V_C_V$C_V) * B_V_C_V$B_V >= RM_limit_1,
+                            "Establish","Fail")
 
 ###
-###############
-###DAY 20###
-############
 
-Fitness_MODEL_PC_20<- lapply(FULL_MODEL_PC, function(x) 
-  subset(as.data.frame(x),
-         as.data.frame(x)$time <= 20))
-
-Fitness_Cut_PC_20_cp <- data.frame(fitness=do.call(rbind,
-                                                   lapply(Fitness_MODEL_PC_20 ,
-                                                          Gametocyte_Fitness,"PC")))
+time_point_of_interest <- c(10,30,50,70,90)
 
 
-Fitness_Cut_PC_20_cp$B_V <- R_C_V$B_V
-Fitness_Cut_PC_20_cp$C_V <- R_C_V$C_V 
-Fitness_Cut_PC_20_cp$Death <- FULL_T_F_PC$TF.V2
+Set_Duration_DF <- Calculate_Set_Duration_CP(FULL_MODEL_PC,
+                          B_V_C_V,
+                          time_point_of_interest)
+              
 
-###A burst size of 21 and transmission investment of 80% is the most
-###optimal
+Grapher_Mortality_DF <- NULL
+for (k in seq(1,length(Set_Duration_DF))){
+  Grapher_Mortality_DF[[k]]<-  
+    cbind.data.frame(grapher_mortality_boundary(Set_Duration_DF[[k]]),
+                                     end_time = time_point_of_interest[[k]])
+}
 
-GG_Fitness_Cut_PC_20_cp <-  ggplot(
-  Fitness_Cut_PC_20_cp, 
-  aes(x=B_V, y= C_V))+
-  geom_raster(aes(fill =fitness))+
-  geom_raster(data=unestablished_index ,aes(x=B_V, y=C_V),
-              fill='#DBDEE8')+
-  geom_raster(data=mort_fitness,aes(x= B_V, y=C_V, fill=
-                                      endfitness))+
-  geom_segment(data=vert_seg_mort, 
-               aes(x=x, xend=xend,
-                   y=y, yend=yend),color='#31f5a3',size=1.2,
-               lineend='round')+
-  geom_segment(data=hor_seg_mort, 
-               aes(x=x, xend=xend,
-                   y=y, yend=yend),color='#31f5a3',size=1.2,
-               lineend='round')+
-  geom_segment(data=vert_seg, 
-               aes(x=x, xend=xend,
-                   y=y, yend=yend),color='black',size=0.8,lineend='round')+
-  geom_segment(data=hor_seg, 
-               aes(x=x, xend=xend,
-                   y=y, yend=yend),color='black',size=0.8,
-               lineend='round')+
-  scale_fill_viridis(name="Cumulative fitness",option='inferno',
-                     limits=c(0,45))+
-  scale_x_continuous(expand=c(0,0))+
-  scale_y_continuous(expand=c(0,0))+
-  xlab(expression("Burst size" ~beta))+
-  ylab("Transmission investment (c)")+
-  ggtitle("B. Day 20")+
-  theme(axis.text= element_text(size=14,color='black'),
-        axis.title= element_text(size=14,color='black'))+
-  annotate("point",x=21, y = 0.80,size=3,col='red')
+Grapher_Mortality_DF<- do.call(rbind, Grapher_Mortality_DF)
+Set_Duration_DF <- do.call(rbind, Set_Duration_DF)
 
 
-#########################################################
-###Day 30- Not Accounting for the post-infection gametocytes#
-#########################################################
-
-Fitness_MODEL_PC_30<- lapply(FULL_MODEL_PC, function(x) subset(as.data.frame(x),
-                                                               as.data.frame(x)$time <= 30))
-
-Fitness_Cut_PC_30_cp <- data.frame(fitness=do.call(rbind,
-                                                   lapply(Fitness_MODEL_PC_30 ,
-                                                          Gametocyte_Fitness,"PC")))
-###Optimal burst size is burst size 13 and and a transmission investmnet of 70%
-
-Fitness_Cut_PC_30_cp$B_V <- R_C_V$B_V
-Fitness_Cut_PC_30_cp$C_V <- R_C_V$C_V 
-Fitness_Cut_PC_30_cp$Death <- FULL_T_F_PC$TF.V2
-
-GG_Fitness_Cut_PC_30_cp<- 
-  ggplot(
-    Fitness_Cut_PC_30_cp, 
-    aes(x=B_V, y= C_V))+
-  geom_raster(aes(fill =fitness))+
-  geom_raster(data=unestablished_index ,aes(x=B_V, y=C_V),
-              fill='#DBDEE8')+
-  geom_raster(data=mort_fitness,aes(x= B_V, y=C_V, fill=
-                                      endfitness))+
-  geom_segment(data=vert_seg_mort, 
-               aes(x=x, xend=xend,
-                   y=y, yend=yend),color='#31f5a3',size=1.2,
-               lineend='round')+
-  geom_segment(data=hor_seg_mort, 
-               aes(x=x, xend=xend,
-                   y=y, yend=yend),color='#31f5a3',size=1.2,
-               lineend='round')+
-  geom_segment(data=vert_seg, 
-               aes(x=x, xend=xend,
-                   y=y, yend=yend),color='black',size=0.8,lineend='round')+
-  geom_segment(data=hor_seg, 
-               aes(x=x, xend=xend,
-                   y=y, yend=yend),color='black',size=0.8,
-               lineend='round')+
-  scale_fill_viridis(name="Cumulative fitness",option='inferno',
-                     limits=c(0,45))+
-  scale_x_continuous(expand=c(0,0))+
-  scale_y_continuous(expand=c(0,0))+
-  xlab(expression("Burst size" ~beta))+
-  ylab("Transmission investment (c)")+
-  ggtitle("C. Day 30")+
-  theme(axis.text= element_text(size=14,color='black'),
-        axis.title= element_text(size=14,color='black'))+
-  annotate("point",x=13, y = 0.70,size=3,col='red')
-
-
-#########################################################
-###Day 50- Not Accounting for the post-infection gametocytes#
-#########################################################
-
-Fitness_MODEL_PC_50<- lapply(FULL_MODEL_PC, function(x) subset(as.data.frame(x),
-                                                               as.data.frame(x)$time <= 50))
-
-Fitness_Cut_PC_50_cp <- data.frame(fitness=do.call(rbind,
-                                                   lapply(Fitness_MODEL_PC_50 ,
-                                                          Gametocyte_Fitness,"PC")))
-
-
-Fitness_Cut_PC_50_cp$B_V <- R_C_V$B_V
-Fitness_Cut_PC_50_cp$C_V <- R_C_V$C_V 
-Fitness_Cut_PC_50_cp$Death <- FULL_T_F_PC$TF.V2
-
-###burst size =44 and transmission investment = 80%
-
-GG_Fitness_Cut_PC_50_cp<- 
-  ggplot(Fitness_Cut_PC_50_cp, 
+GG_Fitness_Set_Duration <- 
+  ggplot(Set_Duration_DF , 
          aes(x=B_V, y= C_V))+
-  geom_raster(aes(fill =fitness))+
-  geom_raster(data=unestablished_index ,aes(x=B_V, y=C_V),
-              fill='#DBDEE8')+
-  geom_raster(data=mort_fitness,aes(x= B_V, y=C_V, fill=
-                                      endfitness))+
-  scale_fill_viridis(name="Cumulative fitness",option='inferno',
-                     limits=c(0,45))+
-  geom_segment(data=vert_seg_mort, 
+  geom_raster(aes(fill = fitness))+
+  geom_segment(data=
+                 Grapher_Mortality_DF, 
                aes(x=x, xend=xend,
                    y=y, yend=yend),color='#31f5a3',size=1.2,
                lineend='round')+
-  geom_segment(data=hor_seg_mort, 
-               aes(x=x, xend=xend,
-                   y=y, yend=yend),color='#31f5a3',size=1.2,
-               lineend='round')+
-  geom_segment(data=vert_seg, 
-               aes(x=x, xend=xend,
-                   y=y, yend=yend),color='black',size=0.8,lineend='round')+
-  geom_segment(data=hor_seg, 
-               aes(x=x, xend=xend,
-                   y=y, yend=yend),color='black',size=0.8,
-               lineend='round')+
+  facet_wrap(~end_time) + 
+  scale_fill_viridis(option='inferno')+
   scale_x_continuous(expand=c(0,0))+
   scale_y_continuous(expand=c(0,0))+
   xlab(expression("Burst size" ~beta))+
-  ylab("Transmission investment (c)")+
-  ggtitle("D. Day 50")+
-  theme(axis.text= element_text(size=14,color='black'),
-        axis.title= element_text(size=14,color='black'))+
-  annotate("point",x=44, y = 0.8, size =3, col='red')
+  ylab("Transmission investment (c)") 
 
-(GG_Fitness_Cut_PC_10_cp+GG_Fitness_Cut_PC_20_cp) /
-  (GG_Fitness_Cut_PC_30_cp+GG_Fitness_Cut_PC_50_cp) +
-  plot_layout(guides='collect')
+
 
 ggsave(here("Figures","Manuscript_Figures",
             "Fitness_Surface_Plot_SD.pdf"), 
